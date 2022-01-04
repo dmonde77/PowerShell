283f35193e122a96b694ec87e4c2a8013b20e2d1
dmonde77 <39627991+dmonde77@users.noreply.github.com>
2022-01-04 19:22:16 +0100

    Update turn-volume-on.ps1

--- Scripts/turn-volume-on.ps1
+++ Scripts/turn-volume-on.ps1
@@ -14,8 +14,6 @@
 try {
 	$obj = new-object -com wscript.shell
 	$obj.SendKeys([char]173)
-	& "$PSScriptRoot/give-reply.ps1" "Audio is on."
-	exit 0 # success
 } catch {
 	"⚠️ Error: $($Error[0]) ($($MyInvocation.MyCommand.Name):$($_.InvocationInfo.ScriptLineNumber))"
 	exit 1
